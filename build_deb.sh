505fe642e3ea77defff3f9bb5727f8e2eaab257d
Pestilence <tvajgert@gmail.com>
2018-08-31 22:12:11 -0500

    update
    - fixed missing requires packages for deb.

--- build_deb.sh
+++ build_deb.sh
@@ -30,4 +30,4 @@ MAINTAINER="mauro.ribeiro@hardkernel.com"
 PKGVER=$(date +%Y%m%d)
 GITREV=$(git rev-parse --short HEAD)
 
-checkinstall --pkgname="cloudshell_lcd" --pkgversion="$PKGVER" -A armhf --maintainer=\"$MAINTAINER\" --pkggroup="other" --pkglicense="GPLv2" --requires="curl,sysstat" --nodoc -y -d2 --pkgrelease="4" -D 
+checkinstall --pkgname="cloudshell_lcd" --pkgversion="$PKGVER" -A armhf --maintainer=\"$MAINTAINER\" --pkggroup="other" --pkglicense="GPLv2" --requires="gawk,samba,samba-common,curl,sysstat" --nodoc -y -d2 --pkgrelease="4" -D
